4656cc8ad1dd04d07a1a906cf71be6c7e4b8c42a
mbruns91 <m.bruns@mpie.de>
2024-01-10 13:18:35 +0100

    updated environment.yml to more recent package versions

--- pyiron_tensile_test/tensile_test_job.py
+++ pyiron_tensile_test/tensile_test_job.py
@@ -2,7 +2,7 @@ from pyiron_base import PythonTemplateJob, DataContainer
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 from scipy.optimize import curve_fit
 import requests
 from SPARQLWrapper import SPARQLWrapper, JSON
